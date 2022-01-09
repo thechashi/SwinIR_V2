7fca502224d1e6aab14d140a6564c1822e00a4fe
chashi-mahiul-islam-bd <ci20l@my.fsu.edu>
2022-01-09 15:06:20 -0500

    fix: 1 channel added to npz file

--- utils/utils_image.py
+++ utils/utils_image.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
+import util_npz as unpz
 
 '''
 # --------------------------------------------
@@ -190,8 +190,10 @@ def imread_uint(path, n_channels=3):
     #  input: path
     # output: HxWx3(RGB or GGG), or HxWx1 (G)
     if n_channels == 1:
-        img = cv2.imread(path, 0)  # cv2.IMREAD_GRAYSCALE
-        img = np.expand_dims(img, axis=2)  # HxWx1
+        img = unpz(path)
+        img= img.reshape((img.shape[0], img.shape[1], 1))
+        #img = cv2.imread(path, 0)  # cv2.IMREAD_GRAYSCALE
+        #img = np.expand_dims(img, axis=2)  # HxWx1
     elif n_channels == 3:
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)  # BGR or G
         if img.ndim == 2:
