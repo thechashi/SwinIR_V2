fb63fcff0981ec41782c68f29bbedd9d0cb7ba28
chashi-mahiul-islam-bd <ci20l@my.fsu.edu>
2022-01-18 15:31:21 -0500

    feat: BGR2RGB commented

--- utils/utils_image.py
+++ utils/utils_image.py
@@ -202,10 +202,12 @@ def imread_uint(path, n_channels=3):
         img2[:,:,2] = img
         img = img2
         #img = cv2.imread(path, cv2.IMREAD_UNCHANGED)  # BGR or G
-        if img.ndim == 2:
-            img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)  # GGG
-        else:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # RGB
+# =============================================================================
+#         if img.ndim == 2:
+#             img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)  # GGG
+#         else:
+#             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # RGB
+# =============================================================================
     return img
 
 
